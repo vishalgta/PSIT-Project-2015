8c502cdf6dc7ba5aa1b574331425977df794724b
belizwp <belizwp@live.com>
2015-11-18 12:37:11 +0700

    add-instructor

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-'''Main of Program'''
+'''
+Main of Program
+=========================
+Parameter Instruction.
+sex    --> 0 == male and femal (default)
+           1 == male only
+           2 == female only
+
+year_0 --> begin range of year. (default=1900)
+year_1 --> end range of year.   (default=2011)
+
+age_0  --> begin range of age.  (default=0)
+age_1  --> end range of age.    (default=119)
+
+'''
 from tool import *
+
+def normal(sex=0):
+    '''create graph from all year.'''
+
+def highest_death(year_0=1900, year_1=2011, age_0, age_1, sex=0):
+    '''create graph of highest death rate.'''
+
+def lowest_death(year_0=1900, year_1=2011, age_0, age_1, sex=0):
+    '''create graph of lowest death rate.'''
