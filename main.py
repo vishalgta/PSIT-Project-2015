6a1d248bcb473d6d94478cdcf7585d021bc0adac
zeanityza <te199@hotmail.com>
2015-12-11 11:19:07 +0700

    create men probability in each age graph, create calculate mean graph of each age men

--- main.py
+++ main.py
@@ -113,4 +113,122 @@ def overall_death_probability_graph(year_0=1900, year_1=2011):
     plt.legend()
     plt.show()
 
-overall_death_probability_graph()
+def calculate_mean_of_each_generation(num, k, mem):
+    """return mean of each generation"""
+    if k <= 3:
+        mem.append(num)
+        if len(mem) == 4:
+            return sum(mem)/4
+    elif k <= 6:
+        mem.append(num)
+        if len(mem) == 3:
+            return sum(mem)/3
+    elif k <= 12:
+        mem.append(num)
+        if len(mem) == 6:
+            return sum(mem)/6
+    elif k <= 19:
+        mem.append(num)
+        if len(mem) == 7:
+            return sum(mem)/7
+    elif k <= 39:
+        mem.append(num)
+        if len(mem) == 20:
+            return sum(mem)/20
+    elif k <= 59:
+        mem.append(num)
+        if len(mem) == 20:
+            return sum(mem)/20
+    elif k <= 119:
+        mem.append(num)
+        if len(mem) == 60:
+            return sum(mem)/60
+
+
+def death_probability_graph_men_in_each_ages():
+    """create death probability graph that points about men in each age for used to know that..
+        0 - 3 baby
+        4 - 6 early childhood
+        7 - 12 middle childhood
+        13 -19 teenager
+        20 - 39 early adulthood
+        40 - 59 middle adulthood
+        60 - 119 old age"""
+    data = load_data(1)
+    date_lis = []
+    baby_list = []
+    early_child_list = []
+    middle_child_list = []
+    teenager_list = []
+    early_adult_list = []
+    middle_adult_list = []
+    old_age_list = []
+    mem = []
+    for i in range(1900, 2012):
+        lis_age = []
+        dates = dt.datetime(i, 1, 1)
+        date_lis.append(dates)
+        lis_age.append(data[i])
+        for k in range(len(lis_age[0])):
+            if k <= 3:
+                values = calculate_mean_of_each_generation(lis_age[0][k], k, mem)
+                if values != None:
+                    baby_list.append(values*100)
+                    mem = []
+            elif k <= 6:
+                values = calculate_mean_of_each_generation(lis_age[0][k], k, mem)
+                if values != None:
+                    early_child_list.append(values*100)
+                    mem = []
+            elif k <= 12:
+                values = calculate_mean_of_each_generation(lis_age[0][k], k, mem)
+                if values != None:
+                    middle_child_list.append(values*100)
+                    mem = []
+            elif k <= 19:
+                values = calculate_mean_of_each_generation(lis_age[0][k], k, mem)
+                if values != None:
+                    teenager_list.append(values*100)
+                    mem = []
+            elif k <= 39:
+                values = calculate_mean_of_each_generation(lis_age[0][k], k, mem)
+                if values != None:
+                    early_adult_list.append(values*100)
+                    mem = []
+            elif k <= 59:
+                values = calculate_mean_of_each_generation(lis_age[0][k], k, mem)
+                if values != None:
+                    middle_adult_list.append(values*100)
+                    mem = []
+            elif k <= 119:
+                values = calculate_mean_of_each_generation(lis_age[0][k], k, mem)
+                if values != None:
+                    old_age_list.append(values*100)
+                    mem = []
+
+    fig, ax = plt.subplots()
+    ax.plot_date(date_lis, baby_list, 'b-', label = 'baby', alpha=3, linewidth=2)
+    ax.plot_date(date_lis, early_child_list, '-', color='#ff3366', label = 'early childhood', alpha=2, linewidth=2)
+    ax.plot_date(date_lis, middle_child_list, '-', label = 'middle childhood', alpha=3, linewidth=2)
+    ax.plot_date(date_lis, teenager_list, '-', label = 'teenager', alpha=3, linewidth=2)
+    ax.plot_date(date_lis, early_adult_list, '-', label = 'early adult', alpha=3, linewidth=2)
+    ax.plot_date(date_lis, middle_adult_list, '-', label = 'middle adult', alpha=3, linewidth=2)
+    ax.plot_date(date_lis, old_age_list, '-', label = 'old age', alpha=3, linewidth=2)
+    plt.rcParams.update({'font.size': 17})
+    plt.xlabel('Year')
+    plt.ylabel('Death Probability (%)')
+    plt.title('Overall of death percent in each years')
+
+    # format the ticks
+    yearsFmt = DateFormatter('%Y')
+    ax.xaxis.set_major_formatter(yearsFmt)
+    ax.autoscale_view()
+    ax.grid(True)
+
+    fig.autofmt_xdate()
+    plt.legend()
+    plt.show()
+death_probability_graph_men_in_each_ages()
+#overall_death_probability_graph(year_0=1900, year_1=2011)
+
+
